2fed548b9b77ce3a8146d58fdc48ce6d0eb7fd83
NurijeHa <nurijehalitii@gmail.com>
2019-10-08 17:29:54 +0200

    no

--- java.java
+++ java.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Java{
 	public static void main(String[] args) {
 	
-	int num1, num2, sum, zb, pj,shum, all;
+	int num1, num2, sum, zb,shum, all, no;
+	double pj;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Number: ");
 		num1 = sc.nextInt();
@@ -16,11 +17,13 @@ public class Java{
 		pj = num1 / num2;
 		shum = num1 * num2;
 		all = num1 * num1 / num1 + num2;
+		no = all;
 		System.out.println("Sum of these numbers: " + sum);	
 		System.out.println("Zb of these numbers: " + zb);
         System.out.println("Pj of these numbers: " + pj);
         System.out.println("Shum of these numbers: " + shum);
-        System.out.println("All of these numbers: " + all);			
+        System.out.println("All of these numbers: " + all);		
+        System.out.println("No of these numbers: " + no);			
 	}
 	
 }
