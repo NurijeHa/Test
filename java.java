fb0eab4bd453dcee6d336a37c5419d3adb73da7a
NurijeHa <nurijehalitii@gmail.com>
2019-10-08 16:10:15 +0200

    add the second java

--- java.java
+++ java.java
@@ -1,12 +1,23 @@
 public class Java{
 	public static void main(String[] args) {
 	
-	int n=5;
-	int j=4;
-	int suma;
-	
-	suma = n + j;
-	System.out.println(suma);
+	int num1, num2, sum, zb, pj,shum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Enter Second Number: ");
+		num2 = sc.nextInt();
+		
+		sc.close();
+		sum = num1 + num2;
+		zb = num1 - num2;
+		pj = num1 / num2;
+		shum = num1 * num2;
+		System.out.println("Sum of these numbers: " + sum);	
+		System.out.println("Zb of these numbers: " + zb);
+        System.out.println("Pj of these numbers: " + pj);
+        System.out.println("Shum of these numbers: " + shum);	
 	}
 	
 }
