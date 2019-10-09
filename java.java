2c148778da295a84e2283b781f7d221b22e1b172
NurijeHa <nurijehalitii@gmail.com>
2019-10-09 12:04:49 +0200

    test testt

--- java.java
+++ java.java
@@ -3,7 +3,7 @@ public class Java{
 	public static void main(String[] args) {
 	
 	int num1, num2, sum, zb,shum, all, no;
-	double pj;
+	double pj, and;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Number: ");
 		num1 = sc.nextInt();
@@ -18,12 +18,14 @@ public class Java{
 		shum = num1 * num2;
 		all = num1 * num1 / num1 + num2;
 		no = all;
+		and = zb;
 		System.out.println("Sum of these numbers: : " + sum);	
 		System.out.println("Zb of these numbers: " + zb);
         System.out.println("Pj of these numbers: " + pj);
         System.out.println("Shum of these numbers: " + shum);
         System.out.println("All of these numbers: " + all);		
-        System.out.println("No of these numbers: " + no);			
+        System.out.println("No of these numbers: " + no);
+        System.out.println("And of these numbers: " + and);		
 	}
 	
 }
